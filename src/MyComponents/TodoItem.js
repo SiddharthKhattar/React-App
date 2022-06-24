aa1581f01b34f452cf8f1998f8bf44cfd3d5b432
SiddharthKhattar <skhattar111@gmail.com>
2022-06-25 00:03:30 +0530

    Initial Stage

---
+++ src/MyComponents/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+      ToDo Works!
+    </div>
+  )
+}
+
+export default Todo
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+      ToDos Works!
+    </div>
+  )
+}
+
+export default Todos
